e5db22c900b701138c4510b43e40ed041c7f67a3
Patrick Augusto Góes de Aguiar Maia <patrick2maia@gmail.com>
2026-03-16 03:58:12 -0300

    feat: mudança de cabeçalho

--- app/src/main/java/com/example/ecoauditpro/telas/DashboardScreen.kt
+++ app/src/main/java/com/example/ecoauditpro/telas/DashboardScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,11 +27,33 @@ fun DashboardScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+
             Text(
                 text = "EcoAudit Pro",
                 fontSize = 32.sp,
+                fontWeight = FontWeight.ExtraBold ,
+                color = Color(0xFF1B5E20)
+            )
+
+            Text(
+                text = "Governance e Compliance",
+                fontSize = 16.sp,
                 fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.primary
+                color = Color.Gray
+            )
+
+            Text(
+                text = "RM562748 - Patrick Maia",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.DarkGray
+            )
+
+            Text(
+                text = "FIAP - 2026",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.DarkGray
             )
 
             Spacer(modifier = Modifier.height(64.dp))
